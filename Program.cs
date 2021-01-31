69050495afbde65b545b7832e0061b8b2b6d1575
Esdra Santos <esdrasantos11@gmail.com>
2021-01-30 21:47:18 -0300

    Projeto iniciado mostrando a mensagem Ola Mundo

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CsharpGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
